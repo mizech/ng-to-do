de59bef26f38ed50ae7f2ce60cae5dca44316ea4
mizech <michael.zech@hotmail.de>
2022-11-22 08:59:12 +0100

    Created class Task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './Task';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-to-do';
+  title = "To Do List";
+  tasks : Task[] = [
+    new Task("Visit Ann", false),
+    new Task("Call Dad", false),
+    new Task("Wash the dishes", false),
+    new Task("Shop for the party", false),
+    new Task("Read some book", false),
+    new Task("Go for a run", false)
+  ];
+
+  add(newTask: string) {
+    this.tasks.push(new Task(newTask));
+  }
+
+  remove(index: number) {
+    var userConfirm = confirm(`Do you want to remove the following task: ${this.tasks[0]}`);
+
+    if (!userConfirm) {
+      return;
+    }
+
+    this.tasks.splice(index, 1);
+  }
+
+  markAsDone(task: Task) {
+    task.isDone = true;
+  }
 }
+
