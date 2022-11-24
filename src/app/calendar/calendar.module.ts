b7996c149eebe0210c42ed1a2810ee004abaf7bf
mizech <michael.zech@hotmail.de>
2022-11-24 07:44:12 +0100

    Added Calendar-CSS

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarComponent } from './calendar.component';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
     CalendarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   exports: [
     CalendarComponent
